0ecfb30c00762ed625240623578ab2af980d6cd8
Diego Masin <diegoifce@gmail.com>
2025-07-18 23:14:02 -0300

    Make a new task to drop database

---
+++ backend/api/management/commands/drop_db.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from django.db import connection
+
+
+class Command(BaseCommand):
+    help = 'Drop all the database tables and create them'
+
+    def handle(self, *args, **kwargs):
+        with connection.cursor() as cursor:
+            # Drop all the tables
+            self.stdout.write(self.style.WARNING('Dropping all tables...'))
+            cursor.execute("DROP SCHEMA public CASCADE;")
+
+            # Recrate the public schema
+            self.stdout.write(self.style.WARNING(
+                'Recreating public schema...'))
+            cursor.execute("CREATE SCHEMA public;")
+
+        self.stdout.write(self.style.SUCCESS('Database reset complete!'))
